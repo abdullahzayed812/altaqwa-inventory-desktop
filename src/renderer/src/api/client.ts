96d4d737cf8cb59a976cbd5a7cf7de32b1688eb8
abdullah <aabdullah.zayed0099@gmail.com>
2026-07-20 04:11:50 +0300

    add dev and produ base url

--- src/renderer/src/api/client.ts
+++ src/renderer/src/api/client.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const BASE_URL = import.meta.env.DEV ? 'http://localhost:3000/api' : 'http://187.124.6.93/api';
+
 const client = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: BASE_URL,
   timeout: 15000,
 });
 
